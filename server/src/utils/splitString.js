d18afa62f493d7cddfc67d8cbe4dff13feb8d626
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2024-02-16 16:33:47 +0000

    Create function that splits debank string into balance and percentage change

---
+++ server/src/utils/splitString.js
@@ -0,0 +1,23 @@
+function splitString(str) {
+  let indexOfPlus = str.indexOf("+");
+  let indexOfMinus = str.indexOf("-");
+
+  let balance, change;
+
+  if (indexOfPlus !== -1) {
+    balance = str.substring(0, indexOfPlus);
+    change = str.substring(indexOfPlus);
+  } else if (indexOfMinus !== -1) {
+    balance = str.substring(0, indexOfMinus);
+    change = str.substring(indexOfMinus);
+  } else {
+    throw new Error("String format not recognized");
+  }
+
+  return {
+    balance: balance,
+    change: change,
+  };
+}
+
+module.exports = { splitString };
