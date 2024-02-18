7211a5dadb535d74679a8a76beac115d894b8ff9
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2024-02-18 23:24:36 +0000

    update evmBalance function to scrape balances for each chain

--- server/src/utils/evmBalance.js
+++ server/src/utils/evmBalance.js
@@ -20,6 +20,37 @@ async function evmBalance(walletAddress) {
   const balancePercent = await page.textContent(
     ".HeaderInfo_totalAssetInner__HyrdC"
   );
+
+  // Wait for page to render and click on div that expands chains displayed
+  await page.waitForSelector(
+    ".AssetsOnChain_item__GBfMt.flex_flexRow__y0UR2.AssetsOnChain_unfoldBtn__ov19o"
+  );
+  await page.click(
+    ".AssetsOnChain_item__GBfMt.flex_flexRow__y0UR2.AssetsOnChain_unfoldBtn__ov19o"
+  );
+
+  // Extract balances for each chain
+  const data = await page.$$eval(
+    ".AssetsOnChain_totalChain__43Xsd > .AssetsOnChain_item__GBfMt",
+    (items) => {
+      return items.map((item) => {
+        const name = item
+          .querySelector(".AssetsOnChain_chainName__jAJuC")
+          ?.textContent.trim();
+        const valueSpan = item.querySelector(".AssetsOnChain_usdValue__I1B7X");
+        const value = valueSpan?.textContent.trim();
+
+        const infoDiv = valueSpan?.parentNode;
+        const allocationMatch = infoDiv?.textContent.match(/(\d+)%/);
+        const allocation = allocationMatch ? allocationMatch[1] : null;
+
+        return { name, value, allocation };
+      });
+    }
+  );
+
+  console.log(data);
+
   let splitObj = splitString(balancePercent);
 
   await browser.close();
@@ -28,6 +59,7 @@ async function evmBalance(walletAddress) {
     address: walletAddress,
     balance: splitObj.balance,
     change: splitObj.change,
+    chains: data,
   };
 
   return evmObj;
