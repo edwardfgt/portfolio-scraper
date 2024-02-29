c30214f9fe6ac699bb96bc0b6d4b72893a879e9c
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2024-02-29 17:32:31 +0000

    Add tailwind Navbar component to client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WalletsDisplay from './components/walletsDisplay';
+import Navbar from './components/navBar';
 
 const App = () => {
   return (
     <Router>
+      <Navbar/>
       <div>
         <Routes>
           <Route path="/" element={<WalletsDisplay />} />
